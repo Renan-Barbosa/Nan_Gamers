491af2020037cdbf551ff3e2b7a0abfcb34287fd
“Renan <nanbarbosa2010@hotmail.com>
2025-04-07 21:41:06 -0300

    modificando a pagina cliente

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelector("form").addEventListener("submit", function (event) {
         event.preventDefault();
+
+        
         })
 });
