25aaf4a1359d7c2b2b81fbf884f6c308cb388c40
“Renan <nanbarbosa2010@hotmail.com>
2025-04-27 21:05:32 -0300

    atualização dos conteudos

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector("form").addEventListener("submit", function (event) {
-        event.preventDefault();
+document.addEventListener("DOMContentLoaded", function (){
+const btnComprar= document.getSelection("btnComprar");
+const btnCadastrarDados= document.getElementById("btnCadastrar");
+let email= document.getElementById("Email");
+let senha= document.getElementById("Senha");
 
-        
-        })
-});
+
+btnCadastrarDados.addEventListener('click', () =>{
+   
+    if(email.value == '' && senha.value == ''){
+      mostrarMensagem("Email ou senha invalidos!");
+         }else{
+      
+      mostrarMensagem("Seja bem vindo!")
+      }
+    
+  });
+  btnComprar.addEventListener('click', () =>{
+    mostrarMensagem("Compra adicionado ao carrinho!")
+  })
+
+
+
+
+  function mostrarMensagem(texto){
+    alert(texto)
+  }
+})
